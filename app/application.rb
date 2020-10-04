1430b34ee0f9a001d893f5f341cc75a3cfc7e485
Justin_Webb <Webb.n.justin@gmail.com>
2020-10-04 14:36:03 -0600

    local app responds based upon time of day

---
+++ app/application.rb
@@ -0,0 +1,16 @@
+require 'date'
+class Application
+    def call(env)
+        resp = Rack::Response.new 
+        current_time = DateTime.now
+    
+        if current_time.hour >= 12
+            resp.write "Good Afternoon!"
+        else
+            resp.write "Good Morning!" 
+        end 
+
+        resp.finish
+    end
+    
+end
